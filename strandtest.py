16dc551df7b4a8156a985c0b033c60dc240bdb16
Terry Ewing <tewing@gracenote.com>
2015-02-08 23:04:10 -0800

    .

--- strandtest.py
+++ strandtest.py
@@ -85,15 +85,15 @@ if __name__ == '__main__':
 	print 'Press Ctrl-C to quit.'
 	while True:
 		# Color wipe animations.
-		colorWipe(strip, Color(255, 0, 0))  # Red wipe
-		colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-		colorWipe(strip, Color(0, 0, 255))  # Green wipe
+		# colorWipe(strip, Color(255, 0, 0))  # Red wipe
+		# colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+		# colorWipe(strip, Color(0, 0, 255))  # Green wipe
 		# Theater chase animations.
 		theaterChase(strip, Color(127, 127, 127))  # White theater chase
-		theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-		theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+		# theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+		# theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
 		# Rainbow animations.
-		rainbow(strip)
-		rainbowCycle(strip)
-		theaterChaseRainbow(strip)
+		# rainbow(strip)
+		# rainbowCycle(strip)
+		# theaterChaseRainbow(strip)
 
